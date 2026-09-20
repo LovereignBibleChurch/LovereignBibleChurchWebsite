aa3b1f3224e9df0bcbb521e6c1375c060ca1e9a9
Grandlhord <elwisdom005@gmail.com>
2026-09-20 11:42:39 +0000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,8 @@ export default async function RootLayout({
           <PartnerBadge />
         </CartProvider>
         <EventCountdownBadge events={events} />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
